de652d957634f3b444ec9f9369a0c49087a03597
SreyPichh <phansreypich2015@gmail.com>
2020-09-02 10:54:57 +0700

    first commit to wordpress theme

---
+++ wp-content/themes/shouei-service/header.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
+	<div class="top-header">
+				<div class="container">
+				<div class="row">	
+					<div class="col-lg-5">
+						<a href="/"><img class="top-logo" src="<?php echo get_template_directory_uri(); ?>\img\logo.png" alt="logo"></a>
+					</div>
+					<div class="col-lg-7">
+						<div class="top-header-contact">
+							<a href="#"> <i class="fa fa-phone"></i> 0462-79-8453</a>
+							<a href="#" class="btn-contact"> お問い合わせ</a>
+						</div>
+					</div>
+				</div>
+					</div>
+			</div>
+		<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-primary" aria-labelledby="main-nav-label">
+
+			
+			<div class="container">
+
+					
+			
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<!-- The WordPress Menu goes here -->
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav ml-auto',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'depth'           => 2,
+						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+					)
+				);
+				?>
+			<?php if ( 'container' === $container ) : ?>
+			</div><!-- .container -->
+			<?php endif; ?>
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- #wrapper-navbar end -->
